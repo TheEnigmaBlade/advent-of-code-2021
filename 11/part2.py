350385929c7622023ea3ce14f0e5cc933f8acb25
Enigma <theenigmablade@gmail.com>
2021-12-11 00:41:35 -0500

    Add day 11 solution

---
+++ 11/part2.py
@@ -0,0 +1,59 @@
+from itertools import count, product
+from pprint import pprint
+
+def load_octo(name: str):
+	with open(f"{name}.txt") as f:
+		return [list(map(int, line.strip())) for line in f.readlines()]
+
+octo_grid = load_octo("input")
+width = 10
+height = 10
+
+def step() -> int:
+	indecies_flashing = set()
+	
+	def update_flash(core_x: int, core_y: int):
+		if core_x < 0 or core_x >= width or core_y < 0 or core_y >= height:
+			return
+		if (core_x, core_y) in indecies_flashing:
+			return
+		# Increment energy (for both the standard increment and flash increment)
+		octo_grid[core_y][core_x] += 1
+		# Cascade flashing
+		if octo_grid[core_y][core_x] > 9:
+			indecies_flashing.add((core_x, core_y))
+			update_flash(core_x - 1, core_y - 1)
+			update_flash(core_x - 1, core_y)
+			update_flash(core_x - 1, core_y + 1)
+			update_flash(core_x, core_y - 1)
+			update_flash(core_x, core_y)
+			update_flash(core_x, core_y + 1)
+			update_flash(core_x + 1, core_y - 1)
+			update_flash(core_x + 1, core_y)
+			update_flash(core_x + 1, core_y + 1)
+	
+	for x, y in product(range(width), range(height)):
+		update_flash(x, y)
+	
+	# Reset flashing
+	for x, y in indecies_flashing:
+		octo_grid[y][x] = 0
+	return len(indecies_flashing)
+
+
+all_flash_time = 0
+
+pprint(octo_grid)
+print()
+
+for i in count():
+	num_flashes = step()
+	print(f"Step {i}: {num_flashes} flashes")
+	pprint(octo_grid)
+	print()
+	
+	if num_flashes == width * height:
+		all_flash_time = i + 1
+		break
+	
+print(f"All flashed after: {all_flash_time}")
